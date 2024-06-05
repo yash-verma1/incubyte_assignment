69f4716f8f037d12f2ba2da45e1cb07d66661c90
Yash Verma <yashv9119@gmail.com>
2024-06-05 17:27:49 +0530

    TDD 1 and 2 numbers in string

--- string_calc.py
+++ string_calc.py
@@ -4,13 +4,23 @@ def add_string(string_to_calc):
     if string_to_calc == '':
         """ Returns 0 if empty string is passed. """
         return 0
-    pass
+    list_of_numbers = map(int, string_to_calc.split(','))
+    return sum(list_of_numbers)
+    
 
 class TestStringCalc(unittest.TestCase):
+    
     def test_empty_string(self):
         result = add_string("")
         self.assertEqual(result, 0)
 
+    def test_one_number_in_string(self):
+        result = add_string("1")
+        self.assertEqual(result, 1)
+
+    def test_two_numbers_in_string(self):
+        result = add_string("1,5")
+        self.assertEqual(result, 6)
 
 if __name__ == '__main__':
     unittest.main()
