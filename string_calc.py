a7c61bdcd37aed0da35f380a1d5eec1ae3679b8e
Yash Verma <yashv9119@gmail.com>
2024-06-05 17:59:13 +0530

    handle custom delims

--- string_calc.py
+++ string_calc.py
@@ -2,14 +2,25 @@ import re
 import unittest
 
 def add_string(string_to_calc):
+    delim = [',', '\n']
     if string_to_calc == '':
         """ Returns 0 if empty string is passed. """
         return 0
-    # split the string_to_calc based on the delimiters (',' , '\n')
-    list_of_numbers = map(int, re.split(r',|\n', string_to_calc))
+    
+    if string_to_calc.startswith('//'):
+        # update the delimiter, its after `//` and before '\n'
+        delim.append(string_to_calc[2:3])
+        # Update the string_to_calc after the newline char
+        string_to_calc = string_to_calc[4:]
+     
+    # string for regex to remove delimiters from string_to_calc
+    delims = '|'.join(delim)
+
+    # split the string_to_calc based on the delimiters
+    list_of_numbers = map(int, re.split(r'{}'.format(delims), string_to_calc))
     
     return sum(list_of_numbers)
-    
+        
 
 class TestStringCalc(unittest.TestCase):
     
@@ -32,6 +43,11 @@ class TestStringCalc(unittest.TestCase):
     def test_new_line_delimiter(self):
         result = add_string("1\n2,3")
         self.assertEqual(result, 6)
+
+    def test_dynamic_delim(self):
+        result = add_string("//;\n1;2")
+        self.assertEqual(result, 3)    
+
 
 if __name__ == '__main__':
     unittest.main()
